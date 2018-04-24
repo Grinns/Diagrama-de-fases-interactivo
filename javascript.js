a5c10c70ff7e006c5c97b68beae2ce1c3c97a5bd
Grinns <almada.pablo.em@gmail.com>
2018-04-24 18:02:39 -0300

    Test

---
+++ javascript.js
@@ -0,0 +1,57 @@
+function unolento()
+        {
+            document.getElementById("img").src = "img/1lento.bmp";
+        }
+function unorapido()
+        {
+            document.getElementById("img").src ="img/1rapido.bmp";
+        }
+function doslento()
+        {
+            document.getElementById("img").src = "img/2lento.bmp";
+        }
+function dosrapido()
+        {
+            document.getElementById("img").src ="img/2rapido.bmp";
+        }
+function treslento()
+        {
+            document.getElementById("img").src = "img/3lento.jpg";
+        }
+function tresrapido()
+        {
+            document.getElementById("img").src ="img/3rapido.jpg";
+        }
+function trescincolento()
+        {
+            document.getElementById("img").src = "img/35lento.jpg";
+        }
+function trescincorapido()
+        {
+            document.getElementById("img").src ="img/35rapido.jpg";
+        }
+
+function cuatrolento()
+        {
+            document.getElementById("img").src = "img/4lento.jpg";
+        }
+function cuatrorapido()
+        {
+            document.getElementById("img").src ="img/4rapido.jpg";
+        }
+function cincolento()
+        {
+            document.getElementById("img").src = "img/5lento.jpg";
+        }
+function cincorapido()
+        {
+            document.getElementById("img").src ="img/5rapido.jpg";
+        }
+function nuevecincolento()
+        {
+            document.getElementById("img").src = "img/95lento.jpg";
+        }
+function nuevecincorapido()
+        {
+            document.getElementById("img").src ="img/95rapido.jpg";
+        }
